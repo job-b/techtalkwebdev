c28ed7aad786d146fd3c1f4c01a54aafcc06b34b
Job <j.becht@ncim.nl>
2016-09-14 00:21:06 +0200

    Removed unnecessary things

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 var paths = {
   scripts: 'app/**/*.js',
@@ -6,4 +8,11 @@ var paths = {
 
 gulp.task('copy', function() {
   gulp.src(paths.scripts).pipe(gulp.dest('build/js'));
+});
+
+gulp.task('build', function() {
+  gulp.src(paths.scripts)
+  	.pipe(concat('all.min.js'))
+  	.pipe(uglify())
+  	.pipe(gulp.dest('build/js'));
 });
