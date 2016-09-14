c4d475bc8df2ff68543dfdf6d8c4a016cded10d7
Job <j.becht@ncim.nl>
2016-09-14 05:14:06 +0200

    Tests now run automatically, no errors

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,21 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var connect = require('gulp-connect')
 var eslint = require('gulp-eslint');
+var Server = require('karma').Server;
+
+
+gulp.task('test', function(done) {
+  return new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, function() {
+  	done();
+  }).start();
+});
+
+gulp.task('autotest', function() {
+  return gulp.watch(['www/js/**/*.js', 'test/spec/*.js'], ['test']);
+});
 
 var paths = {
   html: ['app/**/*.html'],
@@ -34,15 +49,16 @@ gulp.task('build', ['copyHtml', 'copyJSON', 'copyImages'], function() {
 });
 
 gulp.task('ESLint', function() {
-	gulp.src(paths.js)
-		.pipe(eslint())
-        .pipe(eslint.format())
-		.pipe(connect.reload());
+  gulp.src(paths.js)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-	connect.server({
-		livereload: true
-	});
-	gulp.watch(paths.js, ['ESLint']);
+  connect.server({
+	livereload: true
+  });
+  gulp.watch(paths.js, ['ESLint']);
+  gulp.watch(paths.js, ['test']);
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,83 @@
-//jshint strict: false
+// Karma configuration
+// Generated on Wed Sep 14 2016 04:07:33 GMT+0200 (West-Europa (zomertijd))
+
 module.exports = function(config) {
   config.set({
 
-    basePath: './app',
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      '**/*.module.js',
-      '*!(.module|.spec).js',
-      '!(bower_components)/**/*!(.module|.spec).js',
-      '**/*.spec.js'
-    ],
 
-    autoWatch: true,
-
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-    browsers: ['Chrome', 'Firefox'],
 
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine'
-    ]
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-animate/angular-animate.min.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-resource/angular-resource.min.js',
+      'node_modules/angular-route/angular-route.min.js',
+      'node_modules/jquery/dist/jquery.js',
+      'app/app.module.js',
+      'app/app.config.js',
+      'app/app.animations.js',
+      'app/core/core.module.js',
+      'app/core/phone/*.js',
+      'app/core/checkmark/*.js',
+      'app/**/*.module.js',
+      'app/**/*.component.js',
+      'app/**/*.js'
+    ],
 
-  });
-};
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
